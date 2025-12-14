e414b975abcb91304e25072e1ab249b999e10dde
PeterOstr <ostrik_peter@mail.ru>
2025-12-14 19:42:23 +0300

    Remove ignored files from repository

--- cards_schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional
-
-class Card(BaseModel):
-    question: str = Field(..., min_length=3)
-    answer: str = Field(..., min_length=1)
-    source_chunk: int
-    tags: Optional[List[str]] = None
